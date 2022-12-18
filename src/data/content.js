9f6796c38347f787ef49aa9b738d06639de7e399
Ttebify <ttebify@gmail.com>
2022-12-18 10:47:18 +0100

    Update eazypaper link

--- src/data/content.js
+++ src/data/content.js
@@ -1,6 +1,6 @@
 export const menuItem = [
   {
     linkText: "EazyPaper",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLSfFckALoO5qK9PWWJBam9ImkgLxhq-kql2sSL9PToHDHarC0w/viewform",
+    url: "/",
   },
 ];
